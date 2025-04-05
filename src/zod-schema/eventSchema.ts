a71c5733ddbe9414f90ce62cf946945881f58d97
Hoang Duong <hoangduongphantri@gmail.com>
2025-04-06 01:00:13 +0300

    Refactor: Separate event zod schema

--- src/zod-schema/eventSchema.ts
+++ src/zod-schema/eventSchema.ts
@@ -3,6 +3,17 @@ import { z } from "zod";
 export const EventTypeEnum = z.enum(["FOOD", "DRINKS", "ACTIVITY", "OTHER"]);
 export const EventStatusEnum = z.enum(["OPEN", "CLOSED", "CANCELLED"]);
 
+export const eventSchema = z.object({
+  hostId: z.string().min(1, "Host ID is required"),
+  name: z.string().min(1, "Event name is required"),
+  description: z.string().optional(),
+  type: EventTypeEnum.default("FOOD"),
+  date: z.string().transform((str) => new Date(str)),
+  venueId: z.string().min(1, "Venue ID is required"),
+  status: EventStatusEnum.default("OPEN"),
+  capacity: z.number().int().min(1).default(2),
+})
+
 export const eventIdSchema = z.object({
   params: z.object({
     eventId: z.string().min(1, "User ID is required"),
@@ -10,29 +21,12 @@ export const eventIdSchema = z.object({
 });
 
 export const eventCreateSchema = z.object({
-  body: z.object({
-    hostId: z.string().min(1, "Host ID is required"),
-    name: z.string().min(1, "Event name is required"),
-    description: z.string().optional(),
-    type: EventTypeEnum.default("FOOD"),
-    date: z.string().transform((str) => new Date(str)),
-    venueId: z.string().min(1, "Venue ID is required"),
-    status: EventStatusEnum.default("OPEN"),
-    capacity: z.number().int().min(1).default(2),
-  }),
+  body: eventSchema,
 });
 
 export const eventUpdateSchema = z.object({
   params: z.object({
     eventId: z.string().min(1, "Event ID is required"),
   }),
-  body: z.object({
-    name: z.string().min(1).optional(),
-    description: z.string().optional().nullable(),
-    type: EventTypeEnum.optional(),
-    date: z.string().transform((str) => new Date(str)),
-    venueId: z.string().cuid().optional(),
-    status: EventStatusEnum.optional(),
-    capacity: z.number().int().min(1).optional(),
-  }),
+  body: eventSchema.partial(),
 });
